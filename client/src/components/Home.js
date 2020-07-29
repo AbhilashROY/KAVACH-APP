bd2bff525d8076a276489f29d8bfd2a4c66d753d
rajib06081998 <rajeevsaha853@gmail.com>
2020-07-29 15:39:17 +0530

    Node backend with ml integrated

---
+++ client/src/components/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+import InfoCards from "./InfoCards";
+import "../App.css";
+import "../fonts.css";
+import HeatMap from './HeatMap';
+function Home() {
+  return (
+    <React.Fragment>
+      
+      <div id="wrapper2">
+        <div id="welcome" className="container">
+          <div className="title">
+            <h2>Welcome to our website</h2>
+          </div>
+          <p>
+            This is <strong>Kavach</strong>, a free, fully standards-compliant
+            application for rea-time crime reporting.
+          </p>
+        </div>
+      </div>
+      <div id="wrapper3">
+        <div id="portfolio" className="container">
+          <div className="title">
+            <h2>Kavach</h2>
+            <span className="byline">Our Usecases</span>{" "}
+          </div>
+          <InfoCards />
+        </div>
+      </div>
+      <div id="footer" className="container">
+        <div className="title">
+          <h2>Get in touch</h2>{" "}
+        </div>
+        <ul className="contact">
+          <li>
+            <a className="icon icon-phone"></a>
+          </li>
+          <li>
+            <a className="icon icon-twitter"></a>
+          </li>
+          <li>
+            <a className="icon icon-youtube-sign"></a>
+          </li>
+          <li>
+            <a className="icon icon-tumblr"></a>
+          </li>
+        </ul>
+      </div>
+      <HeatMap/>
+    </React.Fragment>
+  );
+}
+
+export default Home;
---
+++ client/src/components/SearchReports.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import axios from 'axios';
+import Typography from "@material-ui/core/Typography";
+
+class SearchReports extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reports: [],
+      isLoaded: false,
+      search: "",
+    };
+  }
+
+  /* This is where data will be fetched from */
+  componentDidMount() {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       reports: result,
+    //     });
+    //   });
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+      console.log(response);
+      let result = response.data;
+      this.setState({
+        isLoaded: true,
+        reports: result,
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+
+  render() {
+    let filteredReports = this.state.reports.filter((report) => {
+      return report.userId.toString().indexOf(this.state.search) !== -1;
+    });
+
+    return (
+      <div>
+        <div id="search-wrapper">
+          <div className="container">
+            <input
+              placeholder="Search"
+              type="text"
+              value={this.state.search}
+              onChange={this.updateSearch.bind(this)}
+            />
+          </div>
+          {filteredReports.map((report) => (
+            <Card key={report.id} style={{ margin: `20px 80px 20px 80px` }}>
+              <div className="root">
+                <CardContent className="contentReports">
+                  <h4>
+                    <strong>title: </strong> {report.title}
+                  </h4>
+                  <h4>
+                    <strong>Reported on: </strong> {report.userId}/
+                    {report.id % 10}/2019
+                  </h4>
+                  <h4>
+                    <strong>Active: </strong> false
+                  </h4>
+                </CardContent>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchReports;
